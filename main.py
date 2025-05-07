2fae27c1241d8e17cb0bd59089064116ba472217
Gabriel Silva Nascimento <gabrielgsn30@gmail.com>
2025-05-07 19:07:18 -0300

    fix update rotas not found

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 async def startup():
             instrumentator = Instrumentator(
                 should_group_status_codes=False,
-                should_ignore_untemplated=True,
+                should_ignore_untemplated=False,
                 should_respect_env_var=False,
                 should_instrument_requests_inprogress=True,
                 excluded_handlers=None,
