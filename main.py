1b90bd767eb7ef07fc408859436dbbd2fd35a87c
Gabrielgsn30 <gabrielgsn30@gmail.com>
2025-05-09 09:36:14 -0300

    alterando titulo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from prometheus_fastapi_instrumentator import (
     metrics,
 )
 
-app = FastAPI(title='API FastAPI - seminarios Docker')
+app = FastAPI(title='FastAPI - seminarios Docker Monitoramento e Automacao')
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
